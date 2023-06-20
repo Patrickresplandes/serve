dfc09cca6b6a3716862ddf34f3514c52983ce529
Patrickresplandes <patrickresplandes87@gmail.com>
2023-06-19 23:08:36 -0300

    colocando o banco de dados em um container

--- src/models/cadastroModel.js
+++ src/models/cadastroModel.js
@@ -1,9 +1,39 @@
 const connection = require('./connection');
 
-const cadastro = async () =>{
-  const query = '';
-  const [cadastroViagem] =  await connection.execute(query,[]);
-    return cadastro;
+const cadastro = async (cadastro) =>{
+  const {	carta_frete,data_carregamento,
+    data_descarregamento,motorista ,placa ,local_carregamento,
+    local_descarregamento ,peso ,valor_tonelada ,valor_frete,
+    comissao ,
+    despesa ,
+    km_inicial, 
+    km_final ,
+    gorjetas ,
+    adiantamentoI, 
+    adiantamentoII, 
+    observacao ,
+    posto ,
+    posto_arla, 
+    litros ,km } = cadastro;
+
+  const query = 'INSERT INTO controle_viagens (carta_frete, data_carregamento, data_descarregamento ,motorista ,placa ,local_carregamento ,local_descarregamento ,peso ,valor_tonelada ,valor_frete ,comissao ,despesa ,km_inicial , km_final ,gorjetas ,adiantamentoI ,adiantamentoII ,observacao ,posto ,posto_arla ,litros ,km) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+  const conn = await connection;
+  const [cadastroViagem] =  await conn.execute(query,
+    [carta_frete,data_carregamento,
+    data_descarregamento,motorista ,placa ,local_carregamento,
+    local_descarregamento ,peso ,valor_tonelada ,valor_frete,
+    comissao ,
+    despesa ,
+    km_inicial, 
+    km_final ,
+    gorjetas ,
+    adiantamentoI, 
+    adiantamentoII, 
+    observacao ,
+    posto ,
+    posto_arla, 
+    litros ,km]);
+    return cadastroViagem;
 };
 
 module.exports = {
--- src/models/connection.js
+++ src/models/connection.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
     host: process.env.MYSQL_HOST,
@@ -7,4 +8,4 @@ const connection = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 });
 
-module.exports = connection
+module.exports = {connection}
