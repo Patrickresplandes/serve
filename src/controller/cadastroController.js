108f94882990a816b688a9c29f6f9652e3c69b47
Patrickresplandes <patrickresplandes87@gmail.com>
2023-06-26 00:18:04 -0300

    feito insert e consulta ao banco

--- src/controller/cadastroController.js
+++ src/controller/cadastroController.js
@@ -1,11 +1,17 @@
+
 const cadastroModel = require('../models/cadastroModel');
 
 const cadastroViagem = async (request, reponse) => {
-    const cadastro = await cadastroModel.cadastro(request.body);
+    const cadastro = await cadastroModel.cadastrarViagem(request.body);
     return reponse.status(201).json(cadastro);
 };
 
+const controleViagem = async (request, response) => {
+    const viagem = await cadastroModel.getViagens();
+    return response.status(200).json(viagem)
+}
 
 module.exports = {
-    cadastroViagem
+    cadastroViagem,
+    controleViagem
 }; 
--- src/router.js
+++ src/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const postController = require('./controller/cadastroController')
+const getController = require('./controller/cadastroController')
 const cadastroMiddleware = require('./middleware/cadastroMiddleware')
 const router = express.Router();
 
 router.post('/cadastro', cadastroMiddleware.validateBody, postController.cadastroViagem);
-router.get('/controle');
+router.get('/controle', getController.controleViagem);
 
 module.exports = router
