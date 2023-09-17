a5c02fdfd7779d2688a7feca0fea41991282f8fc
Patrickresplandes <patrickresplandes87@gmail.com>
2023-09-17 14:59:49 -0300

    feat:api

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 require('dotenv').config();
-const connection = require('./models/connection')
 
-connection.connect().then (() => {
-    const PORT = process.env.PORT || 3333;
-    app.listen(PORT, () => console.log(`Server running or port ${PORT}`));
+const connection = require('./models/connection');
+  
+
+connection.connect().then(() => {
+  const PORT = process.env.PORT || 3333;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
